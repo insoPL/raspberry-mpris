611d181541fb0dbaa9c5e3d51e7ccc153fece0fd
insoPL <huricane.pl.96@gmail.com>
2019-04-29 03:26:28 +0200

    18

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ import time
 class MprisManger:
     def __init__(self):
         self.current_player = ""
-        self.spotifyd_player = MprisController("spotifyd")
-        self.mopidy_player = MprisController("mopidy")
+        self.players = dict()
+        self.players["spotifyd"] = MprisController("spotifyd")
+        self.players["mopidy"] = MprisController("mopidy")
 
     def check_player(self):
-        status = str(self.spotifyd_player.get_status())
-        logging.info(status)
-        status = str(self.mopidy_player.get_status())
-        logging.info(status)
+        for name, player in players:
+            if self.current_player != name and player.get_status == "Playing":
+                self.players[self.current_player].play_pause()
+                self.current_player = name
+        return self.current_player
 
 
 def main():
@@ -22,7 +24,7 @@ def main():
     #next_button = NextButton(40)
     #previous_button = PreviousButton(36)
 
-    mpris_manager = MprisMangager()
+    mpris_manager = MprisManger()
 
     while True:
         print(mpris_manager.check_player())
